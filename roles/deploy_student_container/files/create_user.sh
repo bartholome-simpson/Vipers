cc9a9f14ec8597d76af00b9770dcd3f6d7c92d48
TheHackdes <TheHackdes@thehackdes.com>
2024-05-03 21:56:51 +0200

    :recycle: Refractor code

--- roles/deploy_student_container/files/create_user.sh
+++ roles/deploy_student_container/files/create_user.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-cat <<'EOF' >> /root/.bashrc
+cat <<'EOF' >> /etc/bash.bashrc
+RED="\e[31m"
+STOP="\e[0m"
+
+printf "${RED}"
+figlet -w 1000 Project Viper
+printf "${STOP}"
+printf "Created by: TheHackdes"
+echo -e "\n"
 alias shared='tmux -S /tmp/shareds attach -t shared'
 EOF
 
 cat <<'EOF' >> /etc/ssh/sshd_config
 Match User *
         ForceCommand tmux attach-session -t shared || tmux -S /tmp/shareds new -s shared
-
 EOF
 
 /etc/init.d/ssh restart &> /dev/null
 useradd $1 -m -s /bin/bash -p $(echo "$2" | openssl passwd -1 -stdin)
-
-
-figlet -c ProjectViper | lolcat -F 0.3
 
 echo "Username : ${1}"
 echo "Password : ${2}"
